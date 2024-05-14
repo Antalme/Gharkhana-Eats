6cd4cf4fdc61e4f3f3d38080a050286d0770881d
Antalme <antalmeesantalme@gmail.com>
2024-05-14 22:56:23 +0200

    Haciendo registro. Se necesita almacenar datos personales en MONGO.

--- public/js/registro.js
+++ public/js/registro.js
@@ -1,23 +1,33 @@
+var paso = 1
+
 document.addEventListener('DOMContentLoaded', function () {
     const formulario = document.querySelector('form')
     formulario.addEventListener('submit', function (event) {
-        event.preventDefault();
+        event.preventDefault()
 
-        $.ajax({
-            url: '/registro',
-            method: 'POST',
-            data: {
-                usuario: $("#usuario").val(),
-                email: $("#email").val(),
-                pass: $("#pass").val()
-            },
-            success: function (data) {
-                mostrarMensaje(data)
-            },
-            error: function (error) {
-                console.error(error)
-                mostrarMensaje(error.responseText)
-            }
-        })
+        if (paso == 1) {
+            $("#formularioDatosPersonales").removeClass("oculto")
+        } else {
+            enviarDatosRegistro()
+        }
     })
-});
+})
+
+function enviarDatosRegistro(){
+    $.ajax({
+        url: '/registro',
+        method: 'POST',
+        data: {
+            usuario: $("#usuario").val(),
+            email: $("#email").val(),
+            pass: $("#pass").val()
+        },
+        success: function (data) {
+            mostrarMensaje(data)
+        },
+        error: function (error) {
+            console.error(error)
+            mostrarMensaje(error.responseText)
+        }
+    })
+}
